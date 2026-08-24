e07822d6bc0e205fb8f2c12ab74b19b5a8756520
Thefirstalpha <25564305+Thefirstalpha@users.noreply.github.com>
2026-08-24 23:29:35 +0200

    fix

--- server/logger.ts
+++ server/logger.ts
@@ -22,7 +22,7 @@ const BACKEND_LOG_FORMAT = ':correlationId :type :message';
 
 const MAX_LOG_BUFFER_SIZE = 2000;
 
-const WHITELISTED_HTTP_PATHS = ['GET /api/user', 'GET /api/auth/me', 'GET /api/admin/logs'];
+const WHITELISTED_HTTP_PATHS = new Set(['GET /api/user', 'GET /api/auth/me', 'GET /api/admin/logs', 'GET /api/health']);
 
 export function loggerMiddleware(req: Request, res: Response, next: NextFunction) {
   const correlationId = randomUUID();
@@ -70,7 +70,7 @@ export class Logger {
 
   static http(req: Request, res: Response, duration: number) {
     if (
-      WHITELISTED_HTTP_PATHS.includes(`${req.method} ${req.originalUrl}`) &&
+      WHITELISTED_HTTP_PATHS.has(`${req.method} ${req.originalUrl}`) &&
       res.statusCode < 400
     ) {
       return;
